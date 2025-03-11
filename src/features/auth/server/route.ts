47b3fb84459ee23ab1e6fd49e4c56805aa3d0f9f
soumojit622 <soumojitbanerjee22@gmail.com>
2025-03-11 23:10:11 +0530

    🔐 Building session middleware for authentication

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,12 +1,17 @@
 import { Hono } from "hono";
-import { loginSchema, registerSchema } from "../schemas";
 import { zValidator } from "@hono/zod-validator";
-import { deleteCookie, setCookie } from "hono/cookie";
-import { AUTH_COOKIE } from "../constants";
+import { loginSchema, registerSchema } from "../schemas";
 import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -21,7 +26,7 @@ const app = new Hono()
       maxAge: 60 * 60 * 24 * 30,
     });
 
-    return c.json({ succes: true });
+    return c.json({ success: true });
   })
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
@@ -38,6 +43,14 @@ const app = new Hono()
       sameSite: "strict",
       maxAge: 60 * 60 * 24 * 30,
     });
+
+    return c.json({ success: true });
+  })
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = await c.get("account");
+
+    deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
 
     return c.json({ success: true });
   });
